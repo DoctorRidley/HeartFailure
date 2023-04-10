cf7b1d299936f2efb40fbb758905f6d4a7eb0684
DoctorRidley <18543868+DoctorRidley@users.noreply.github.com>
2023-04-10 01:21:13 -0700

    Adds some Spark config

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -1,23 +1,28 @@
-import clients.HiveClient
-import models.Patient
-
-
 import scala.io.StdIn.readLine
+
+import org.apache.spark.SparkContext;
+import org.apache.spark.SparkConf;
+import org.apache.spark.rdd.RDD;
 
 object Driver {
 
+
     def main(args: Array[String]): Unit = {
 
-        HiveClient.Connect()
+        val conf: SparkConf = new SparkConf()
+          .setAppName("Heart Failure")
+          .setMaster("local");
+
+        val sc: SparkContext = new SparkContext(conf);
+
+        val rdd: RDD[String] = sc.textFile("resources/heart.csv");
+
+        println(rdd.getClass);
+
+        println("Collected CSV file")
 
 
-
-
-
-        HiveClient.Close()
-
-
-
+        println("\n\n\n");
 
 
     }
